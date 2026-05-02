8ea6ad2349da07af9f30aec3b69045e4dbe777cf
Vitaly <obergodmar@gmail.com>
2026-05-03 02:09:12 +0300

    feat(ci): add ci

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,33 @@
-import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'path';
 
+import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
-import viteTsConfigPaths from 'vite-tsconfig-paths';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
+  resolve: {
+    tsconfigPaths: true,
+  },
   server: {
     port: 4200,
   },
   plugins: [
     react(),
-
-    viteTsConfigPaths({
-      root: './',
-      parseNative: true,
+    dts({
+      include: ['src/scrollbar'],
+      outDir: 'dist',
+      tsconfigPath: 'tsconfig.json',
     }),
   ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/scrollbar/index.ts'),
+      formats: ['es'],
+      fileName: 'index',
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+    },
+    copyPublicDir: false,
+  },
 });
