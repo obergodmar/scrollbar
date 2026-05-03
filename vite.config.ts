07c49bc0c14d5986f612e5388aa79ff97fbb735a
Vitaly <obergodmar@gmail.com>
2026-05-03 16:27:18 +0300

    feat(package)!: change scope add gh release and update builder deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { resolve } from 'path';
 
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
